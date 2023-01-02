24200b50c2422b729c0b806084e05de5957c19b3
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-02 13:53:42 -0700

    update h1 tag

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -50,7 +50,7 @@ export default function Home({ data }) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          {posts[0].attributes.title}
+          9d8.dev
         </h1>
 
         <div className={styles.grid}>
@@ -62,19 +62,6 @@ export default function Home({ data }) {
           })}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
