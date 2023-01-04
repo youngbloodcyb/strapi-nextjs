2959317acd02e2acb40b293cfae9754bdad8c2a6
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-03 21:34:41 -0700

    delete test function

--- frontend/lib/test.js
+++
@@ -1,6 +0,0 @@
-export async function test({ title, description, body, slug }) {
-    console.log(title);
-    console.log(description);
-    console.log(body);
-    console.log(slug);
-}
--- frontend/pages/api/blog.js
+++ frontend/pages/api/blog.js
@@ -6,7 +6,6 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
         const payload = req.body;
         const response = await postBlog(payload);
-        // test(payload);
 
         res.status(200).json({
             status: 'success',
