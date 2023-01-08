0323d837b934a935a4de753a7585a1fb3bd127a0
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-08 08:35:28 -0700

    update styles across blog component and index

--- frontend/components/Blog.js
+++ frontend/components/Blog.js
@@ -4,28 +4,28 @@ import Link from "next/link";
 export default function Blog({title, slug, description, publishedAt}) {
     const date = new Date(publishedAt).toLocaleDateString();
     return (
-        <Link href={`/posts/${slug}`}>
-            <div className="bg-gray-800 text-gray-100 rounded-lg">
-                <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+        <div className="text-black bg-white rounded-lg transition-all duration-300 ease-in-out hover:p-2 border border-black">
+            <Link href={`/posts/${slug}`}>
+                <div className="max-w-4xl px-10 py-6 mx-auto rounded-lg bg-white">
                     <div className="flex items-center justify-between">
-                        <span className="text-sm text-gray-400">{date}</span>
-                        <div className="px-2 py-1 font-bold rounded dark:bg-violet-400 dark:text-gray-900">Javascript</div>
+                        <span className="text-sm">{date}</span>
+                        <div className="px-2 py-1 uppercase rounded text-gray-900">#LABEL</div>
                     </div>
                     <div className="mt-3">
                         <h2 className="text-2xl font-bold">{title}</h2>
                         <p className="mt-2">{description}</p>
                     </div>
                     <div className="flex items-center justify-between mt-4">
-                        <span className="text-violet-400">Read more</span>
+                        <span className="">Read more</span>
                         <div>
                             <div href="#" className="flex items-center">
-                                <span className="text-gray-400">Author: Contributor</span>
+                                <span className="">Author Contributor</span>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
