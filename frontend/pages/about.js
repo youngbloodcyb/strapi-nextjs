fd8731f28e5761197b0d3ccb57505238eaec022d
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-16 22:30:25 -0700

    update about route

--- frontend/pages/about.js
+++
@@ -1,8 +0,0 @@
-import Nav from "../components/Nav";
-
-export default function About() {
-    <div className="mx-12 md:mx-24">
-        <Nav/>
-        
-    </div>
-}
---
+++ frontend/pages/about/index.js
@@ -0,0 +1,10 @@
+import Nav from "../../components/Nav";
+
+export default function About() {
+    return (
+        <div className="mx-12 md:mx-24">
+            <Nav/>
+        </div>
+    )
+    
+}
