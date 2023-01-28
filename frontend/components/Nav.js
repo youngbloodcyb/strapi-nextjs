c13d5723ac695245bdc44a4fa8816d08813b7577
youngbloodcyb <cameron.youngblood@gmail.com>
2023-01-28 09:53:25 -0700

    make minor UI changes to nav component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -18,8 +18,8 @@ export default function Nav() {
                 >
                 <path
                     d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     strokeWidth="2"
                 ></path>
                 </svg>
